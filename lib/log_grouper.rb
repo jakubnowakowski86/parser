9869be6373104137ae697462e5edb054c4cde47a
Jakub Nowakowski <jakubnowakowski86@gmail.com>
2021-03-09 10:58:46 +0100

    Little clean up

--- lib/log_grouper.rb
+++ lib/log_grouper.rb
@@ -1,11 +1,11 @@
 class LogGrouper
   def call(logs, options = {})
-    prepared_options = prepare_options(options)
+    group_by_column, uniqueness = prepare_options(options)
     collection = Hash.new { |hash, key| hash[key] = [] }
     logs.each do |log|
       split_log = log.split(' ')
-      key = split_log.delete(split_log[prepared_options[:group_by_column]])
-      collection[key].concat(split_log) unless prepared_options[:uniqueness] && collection[key].include?(split_log[0])
+      key = split_log.delete(split_log[group_by_column])
+      collection[key].concat(split_log) unless uniqueness && collection[key].include?(split_log[0])
     end
     collection
   end
@@ -13,9 +13,9 @@ class LogGrouper
   private
 
   def prepare_options(raw_options)
-    {}.tap do |options|
-      options[:group_by_column] = raw_options.fetch(:group_by_column, 1)
-      options[:uniqueness] = raw_options.fetch(:uniqueness, true)
-    end
+    [
+      raw_options.fetch(:group_by_column, 1),
+      raw_options.fetch(:uniqueness, true)
+    ]
   end
 end
